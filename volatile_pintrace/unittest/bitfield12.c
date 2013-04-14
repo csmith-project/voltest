31968aeeea9b3cccbe483da1c9099846bc52a4fb
Yang Chen <chenyang@cs.utah.edu>
2013-04-14 07:14:58 +0000

    new test cases

---
+++ volatile_pintrace/unittest/bitfield12.c
@@ -0,0 +1,23 @@
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const volatile unsigned f4 : 30;
+   unsigned f5 : 31;
+   volatile signed f6 : 1;
+   volatile signed f7 : 1;
+};
+#pragma pack(pop)
+
+static struct S0 g1;
+
+unsigned int foo(void)
+{
+  return  g1.f4 + g1.f5 + g1.f6 + g1.f7;
+}
+
+int main(void)
+{
+  foo();
+  return 0;
+}
+
